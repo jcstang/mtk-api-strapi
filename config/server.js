c98d616b7cc4f39d21c6ed2d47dcba2ecf479285
Jacob Stanger <devjake@outlook.com>
2021-05-28 14:33:42 -0600

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '754660484b918e4b9bafec056cc27192'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2839967-6ce3-48af-9785-0ef781c1a229'
+};
